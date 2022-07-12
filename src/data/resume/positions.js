5261a53d94c554f284a4e59e1dc52333006bffbd
Alan  Matthew <alan.matthew.anggara@gmail.com>
2022-07-13 03:18:57 +0800

    updated finance resume

--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -3,7 +3,7 @@ const positions = [
     company: 'Investree',
     position: 'Fullstack Engineer',
     link: 'https://investree.id/',
-    daterange: 'March 2022 - Present',
+    daterange: 'Jan 2022 - Present',
     points: [
       'Designed and implemented new features using VueJS to visualize data stored from APIs, decreasing the time needed by users to find important information about their investments.',
       'Designed and deployed RESTful APIs that served data to the front-end based on dynamically chosen user inputs that handled over 100,000 users using Kotlin.',
@@ -14,27 +14,31 @@ const positions = [
     company: 'Singular Asset Management',
     position: 'Equity Analyst',
     link: 'https://singular.com.my/',
-    daterange: 'November 2021 - January 2022',
+    daterange: 'Sep 2021 - Dec 2022',
     points: [
-      'Recommended long-term trading opportunities on the Indonesian stock exchange to my portfolio manager and CIO.',
-      'Performed company analysis, industry research, competitor analysis, and DCF modelling on target companies for internal research and client presentation.',
+      'Recommended long-term trading opportunities on the Indonesian stock exchange to my portfolio manager and CIO, with the goal of delivering attractive risk-adjusted returns to investors.',
+      'Performed company analysis, industry research and competitor analysis to identify trends in the industry.',
+      'Conducted DCF analysis on target companies to identify undervalued opportunities.'
     ],
   },
   {
     company: 'Kredens Capital Management',
     position: 'Trading Assistant',
     link: 'https://kredenscapital.com/',
-    daterange: 'March 2021 - July 2021',
+    daterange: 'Feb 2021 - July 2021',
     points: [
-      'Recommended short-term trading opportunities on the NASDAQ, NYSE, and LSE stock exchanges to my portfolio manager and CIO.',
-      'Performed company analysis, industry research, competitor analysis, and DCF modelling on target companies for internal research and client presentations.',
+      'Recommended short-term trading opportunities on the NASDAQ, NYSE, and LSE stock exchanges to my portfolio manager and CIO using fundamental analysis and technical analysis, generating a 40% increase in profits from my ideas.',
+      'Executed trades through multiple channels including electronic execution platforms (e.g., Bloomberg)',
+      'Monitored positions daily for opportunities to adjust exposure based on changing market conditions and updated investor reports regularly regarding performance statistics and portfolio holdings.',
+      'Provided analytical support to other members of the Investment Team by developing new trading strategies, analyzing existing portfolios, conducting research related to specific industries or companies, etc'
+      
     ],
   },
   {
     company: 'National University of Singapore, Office of Environmental Sustainability',
     position: 'Student Associate',
     link: 'https://sustainability.nus.edu.sg/',
-    daterange: 'September 2020 - February 2021',
+    daterange: 'September 2020 - Jan 2021',
     points: [
       'Tasked with collecting data on recycling rates of a newly developed recycling bin design developed by the College of Engineering',
       'Performed statistical analysis on the data using R and consolidated my findings into a pitch deck for my Associate Director.',
